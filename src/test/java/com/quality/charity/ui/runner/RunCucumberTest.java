f2cbdc350c90cd47bb360e2d003db71d104e72a2
SN95LA <London_1234>
2023-01-04 10:15:34 +0530

    refct

--- src/test/java/com/quality/charity/ui/runner/RunCucumberTest.java
+++ src/test/java/com/quality/charity/ui/runner/RunCucumberTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue = "com.ing.thor.ui.steps", features = "src/test/resources")
+@CucumberOptions(glue = "com.quality.charity.ui.steps", features = "src/test/resources")
 public class RunCucumberTest {
 
 }
--- src/test/java/com/quality/charity/ui/webdriver/factory/configs/SpringConfiguration.java
+++ src/test/java/com/quality/charity/ui/webdriver/factory/configs/SpringConfiguration.java
@@ -2,7 +2,7 @@ package com.quality.charity.ui.webdriver.factory.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.ing.thor.ui")
+@ComponentScan(basePackages = "com.quality.charity.ui")
 public class SpringConfiguration {
 
 }
