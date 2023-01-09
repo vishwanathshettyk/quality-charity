de2ebcf037fadb2ca91793417a28d091124ed0af
VishwanathShetty <London_1234>
2023-01-09 15:42:12 +0530

    fix/docker file added

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# create/pull image using :  docker build -t quality-charity:latest .
+# run the container :  docker run -it --name quality-charity-container quality-charity:latest /bin/bash
+# now you can run test using run :  mvn clean test (runs only on chrome now)
+
+
+#
+# Build stage
+#
+FROM --platform=linux/amd64 markhobson/maven-chrome:jdk-8
+WORKDIR /Quality-Charity
+COPY src /Quality-Charity/src
+COPY pom.xml /Quality-Charity
+RUN mvn -f /Quality-Charity/pom.xml clean package -Dmaven.test.skip=true
--- src/main/java/com/quality/charity/factory/local/ChromeManager.java
+++ src/main/java/com/quality/charity/factory/local/ChromeManager.java
@@ -2,14 +2,19 @@ package com.quality.charity.factory.local;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChromeManager {
 
+
     public static WebDriver getDriver()
     {
-        return new ChromeDriver();
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--no-sandbox", "--headless","--disable-dev-shm-usage");
+        return new ChromeDriver(options);
     }
 
 }
